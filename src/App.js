045c9a81fe190e9aa168bcc78a309b8f22634bf6
Brandon <Bphamnldb@gmail.com>
2023-03-06 22:15:14 -0800

    objects

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ const App = () => {
       <h1>Greetings</h1>
       <Hello name='Maya' age={26 + 10} />
       <Hello name={name} age={age} />
-
+      <Footer />
+      <Friends />
     </div>
   )
 }
@@ -25,5 +26,30 @@ const Hello = (props) => {
   )
 }
 
+const Footer = () => {
+  return (
+    <>
+      greeting app created by < a href='httpsL//github.com/mluukkai' > mluukkai </a >
+    </>
+  )
+}
 
+const Friends = () => {
+  const array = [
+    { name: 'Peter', age1: 4 },
+    { name: 'Maya', age1: 10 },
+  ]
+  const friends = ['peter', 'maya', 2]
+
+  return (
+    <>
+      <p>{array[0].name} {array[0].age1}</p>
+      <p>{array[1].name} {array[1].age1}</p>
+      <p>{friends}</p>
+    </>
+
+
+
+  )
+}
 export default App
