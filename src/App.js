77884804624e0d484f07a5c1a6945828e82a2cfb
Brandon <Bphamnldb@gmail.com>
2023-03-06 21:26:08 -0800

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-)
+
+
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+  console.log("hi")
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name='Maya' age={26 + 10} />
+      <Hello name={name} age={age} />
+
+    </div>
+  )
+}
+
+const Hello = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  )
+}
+
 
 export default App
